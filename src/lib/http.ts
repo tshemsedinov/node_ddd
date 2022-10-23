1b02a840d792c4abc2a86ffacb9b853a9d27a1a8
vova <vladimir.gubarenko@gmail.com>
2022-10-23 11:28:03 +0300

    api refactoring

--- src/lib/http.ts
+++ src/lib/http.ts
@@ -1,32 +1,44 @@
 'use strict';
 
 import http from 'node:http';
-import logger from './logger'
+import logger from './logger';
+
+const HEADERS = {
+  'X-XSS-Protection': '1; mode=block',
+  'X-Content-Type-Options': 'nosniff',
+  'Strict-Transport-Security': 'max-age=31536000; includeSubdomains; preload',
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'Content-Type': 'application/json; charset=UTF-8',
+};
 
 const receiveArgs = async (req: any) => {
   const buffers = [];
   for await (const chunk of req) buffers.push(chunk);
   const data = Buffer.concat(buffers).toString();
-  return JSON.parse(data);
+  return data ? JSON.parse(data) : null;
 };
 
 export default (routing: Record<string, any>, port: number) => {
   http.createServer(async (req: Record<string, any>, res: Record<string, any>) => {
+    res.writeHead(200, HEADERS);
+    if (req.method !== 'POST') return res.end('"Only POST allowed"');
     const { url, socket } = req;
-    const [name, method, id] = url.substring(1).split('/');
-    const entity = routing[name];
+    const [place, name, method, id] = url.substring(1).split('/');
 
+    if (place !== 'api') return res.end('"Not found place"');
+    if (!name || !routing[name]) return res.end('Not found route');
+    if (!method || !routing[name][method]) return res.end('Not found method');
 
-
-    if (!entity) return res.end('Not found route');
-    const handler = entity[method];
-    if (!handler) return res.end('Not found method');
-
-    // TODO method and arg from req type GET, POST
     const args: string[] = [];
-    args.push(id);
+    if (id) args.push(id);
+    const data = await receiveArgs(req);
+    if (data) args.push(data);
 
-    logger.log(`${socket.remoteAddress} ${method} ${url}`);
+    logger.log(`${req.method} ${socket.remoteAddress} ${method} ${url}`);
+    logger.log(args);
+
     const result = await routing[name][method](...args);
     res.end(JSON.stringify(result.rows));
   }).listen(port);
