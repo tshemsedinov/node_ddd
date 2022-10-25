bb6f851c5f173f65aabed3e7ab15ce16528df48c
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2022-10-25 18:32:50 +0300

    Update load.ts

--- src/lib/load.ts
+++ src/lib/load.ts
@@ -1,24 +1,20 @@
-'use strict';
-
 import { promises as fsp } from 'node:fs';
 import path from 'node:path';
 import vm from 'node:vm';
 import logger from './logger';
 import config from '../config'
 
-export class Load {
-  public static async loadApiRoutes(sandbox: Object, pathParam: string, libParam: string) {
-    const result: Record<string, any> = {};
-    for (const fileName of (await fsp.readdir(path.join(__dirname, pathParam)))) {
-      if (!fileName.endsWith('.ts') || fileName.endsWith('base.ts')) continue;
-      const apiName = path.basename(fileName, '.ts');
-      result[apiName] = vm.runInContext('new Api(db, common);',
-        vm.createContext(Object.freeze({
-          ...sandbox,
-          Api: (await import(pathParam + apiName))['default']
-        })), { ...config.sandbox });
-      logger.log('Load API: ' + apiName);
-    }
-    return result;
+export async loadApiRoutes(sandbox: Object, pathParam: string, libParam: string) {
+  const result: Record<string, any> = {};
+  const files = await fsp.readdir(path.join(__dirname, pathParam));
+  for (const fileName of files) {
+    if (!fileName.endsWith('.ts') || fileName.endsWith('base.ts')) continue;
+    const apiName = path.basename(fileName, '.ts');
+    const Api = await import(pathParam + apiName)).default;
+    const context = Object.freeze({ ...sandbox, Api });
+    vm.createContext(context, { ...config.sandbox });
+    result[apiName] = vm.runInContext('new Api(db, common);', context);
+    logger.log('Load API: ' + apiName);
   }
+  return result;
 }
